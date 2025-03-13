b3d90ce57d0bcc398ae6a24fc79323fb28b97827
CeloDigital <celoartdigital@gmail.com>
2025-03-13 13:03:40 -0300

    criado o componente footer e foram adicionados os arquivos jsx e css

---
+++ SAS/src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import '../Footer/footer.css'
+
+export default function Footer() {
+    
+    return (
+        <footer>
+            <div className='footer'>
+                &copy; TODOS OS DIREITOS RESERVADOS
+            </div>
+        </footer>
+    )
+}
