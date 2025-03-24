bec4ac81c32b2e65d836396b3f5b15c377c2b9f2
CeloDigital <celoartdigital@gmail.com>
2025-03-24 10:17:55 -0300

    Feat - Implementações das telas de Redefinição - novos icones adicionados e Atualizações na página de login para implementação das telas de Redefinição de senha

--- SAS/src/App.jsx
+++ SAS/src/App.jsx
@@ -3,6 +3,12 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import Cadastro from './pages/Cadastro'
+import Email from './components/Tokenemail'
+import Autenticacao from './components/Tokenautenticacao'
+import Redefinir from './components/RedefinirSenha'
+import Feedback from './components/Feedback'
+
+
 
 
 function App() {
@@ -10,12 +16,19 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-
         <Route path='/' element={<Home />}></Route>
 
         <Route path='/login' element={<Login />}></Route>
 
         <Route path='/cadastro' element={<Cadastro />}></Route>
+
+        <Route path='/aut' element={<Autenticacao />}></Route>
+
+        <Route path='/emaileverify' element={<Email />}></Route>
+
+        <Route path='/redesenha' element={<Redefinir />}></Route>
+
+        <Route path='/feedback' element={<Feedback />}></Route>
 
       </Routes>
     </BrowserRouter>
--- SAS/src/pages/Login/index.jsx
+++ SAS/src/pages/Login/index.jsx
@@ -1,24 +1,49 @@
-import Button from '../../components/Navbar'
-import Logo from '../../assets/images/logo-sas.svg'
-import Img_login from '../../assets/images/imagem-login.svg'
+import Button from '../../components/Navbar';
+import Logo from '../../assets/images/logo-sas.svg';
+import Img_login from '../../assets/images/imagem-login.svg';
 
-import { Navigate, useNavigate } from "react-router"
+import { Navigate, useNavigate } from "react-router";
 import { useState } from "react";
 
-import './login.css'
+import './login.css';
+import Email from '../../components/Tokenemail';
+import Autenticacao from '../../components/Tokenautenticacao';
+import Redefinir from '../../components/RedefinirSenha';
+import Feedback from '../../components/Feedback';
 
 export default function Login() {
 
-    const navigation = useNavigate()
+    const navigation = useNavigate();
 
-    const [isOpen, setIsOpen] = useState (false)
+    const [isEmailOpen, setIsEmailOpen] = useState(false);
+    const [isAuthOpen, setIsAuthOpen] = useState(false);
+    const [isSenhaOpen, setIsSenhaOpen] = useState(false);
+    const [isFeedbackOpen, setIsFeedbackOpen] = useState(false);
+    
 
-    const toggleMenu = () => {
-        setIsOpen(!isOpen)
-    }
+    const togglePasswordReset = (e) => {
+        e.preventDefault();
+        setIsEmailOpen(!isEmailOpen);
+    };
+
+    const handleEmailClose = () => {
+        setIsEmailOpen(false);
+        setIsAuthOpen(true);
+    };
+
+    const handleAuthClose = () => {
+        setIsAuthOpen(false);
+        setIsSenhaOpen(true);
+    };
+
+    const handleSenhaClose = () => {
+        setIsSenhaOpen(false);
+        setIsFeedbackOpen(true);
+    };
+
     return (
         <>
-            <Button/>
+            <Button />
             <section className="container">
                 <div className='detalhe_login'></div>
                 <div className='detalhe_login2'></div>
@@ -26,7 +51,7 @@ export default function Login() {
                     <img className='imgForm' src={Img_login} alt="" />
                     <div className="login">
                         <div className='logo_login'>
-                            <img src={Logo} alt="" width={57.75} height={66}/>
+                            <img src={Logo} alt="" width={57.75} height={66} />
                         </div>
                         <form action="">
                             <h2 id='adp'>Área do Paciente</h2>
@@ -35,13 +60,17 @@ export default function Login() {
                             <input id='email' type="e-mail" placeholder="Digite seu e-mail" /><br />
                             <label htmlFor="senha">Senha:</label><br />
                             <input id='senha' type="password" placeholder="Digite sua senha" />
-                            <p id='ems'><a href="">Esqueci minha senha</a></p>
+                            <p id='ems'><a href="#" onClick={togglePasswordReset}>Esqueci minha senha</a></p>
                             <button id='button_login'>Entrar</button>
                             <p id='ntc'>Não tem uma conta? <a href="" onClick={() => navigation('/cadastro')}>Cadastre-se</a></p>
                         </form>
                     </div>
                 </div>
+                {isEmailOpen && <Email onClose={handleEmailClose} />}
+                {isAuthOpen && <Autenticacao onClose={handleAuthClose} />}
+                {isSenhaOpen && <Redefinir onClose={handleSenhaClose} />}
+                {isFeedbackOpen && <Feedback onClose={() => setIsFeedbackOpen(false)} />}
             </section>
         </>
-    )
+    );
 }
