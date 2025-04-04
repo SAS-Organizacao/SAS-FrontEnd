df26aa79ff42787c80f88840c3521d3f077b5859
dorot.rodrigues <dorotbarbosa57@gmail.com>
2025-04-04 00:12:36 -0300

    Criação dos componentes Barra-med, Navbar-logada, atualização no componente navbar e criação da tela Agendar_exame, faltando alguns ajustes para finalizá-la.

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,12 @@ import Email from "./components/Tokenemail";
 import Autenticacao from "./components/Tokenautenticacao";
 import Redefinir from "./components/RedefinirSenha";
 import Feedback from "./components/Feedback";
+import NavbarLogada from './components/Navbar_logada';		
+import Barra_med from './components/Barra-med';
+import Resultado_exames from './pages/Resultado_exames';
+import Histórico_consultas from './pages/Histórico_consultas';
+import Info_prontuario from './components/Info_prontuario';
+import Agendar_exames from './pages/Agendar_exames';
 
 import Menu_lateral from "./components/Menu_lateral";
 import Agendar_consulta from "./components/Agendar_consulta";
@@ -15,6 +21,7 @@ import Perfil from "./pages/Perfil";
 import Barra from "./components/Barra";
 import AreaPaciente from "./pages/Area_paciente/areapac";
 import Sobre from "./pages/Sobre/sobre";
+import Botoes_prontuario_med from './components/Botoes_prontuario_med';
 
 function App() {
   return (
@@ -42,6 +49,21 @@ function App() {
         <Route path="/areapaciente" element={<AreaPaciente />}></Route>
         <Route path="/perfil" element={<Perfil />}></Route>
         <Route path="/agendarconsulta" element={<Agendar_consulta />}></Route>
+
+        <Route path='/resultadoexames' element={<Resultado_exames/>} ></Route>
+        
+        <Route path='/historicoconsultas' element={<Histórico_consultas/>} ></Route>
+
+        <Route path='/infoprontuario' element={<Info_prontuario/>} ></Route>
+
+        <Route path='/botoes_prontuario_med' element={<Botoes_prontuario_med/>} ></Route>
+
+        <Route path='/agendar_exames' element={<Agendar_exames/>} ></Route>
+
+        <Route path='/navbar_logada' element={<NavbarLogada/>} ></Route>
+
+        <Route path='/barra_med' element={<Barra_med/>} ></Route>
+
       </Routes>
     </BrowserRouter>
   );
